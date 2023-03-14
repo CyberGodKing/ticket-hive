b641a099e4669386a0f2133903ff2545bb987c28
Android_Lucifer <89004415+CyberGodKing@users.noreply.github.com>
2023-03-15 00:19:43 +0100

    Update settings.py

--- Ticket/settings.py
+++ Ticket/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qvp&bngan3%we+w#weu$84odayl%d3s5!5*dghx0j82!3p%1-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ticket-hive-production.up.railway.app","127.0.0.1"]
 
 
 # Application definition
@@ -136,3 +136,4 @@ PAYSTACK_PUBLIC_KEY ="pk_live_e21085931c9a4bbdf0eded9ef8152a41f630cd8d"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
+CSRF_TRUSTED_ORIGINS =["https://ticket-hive-production.up.railway.app"]
